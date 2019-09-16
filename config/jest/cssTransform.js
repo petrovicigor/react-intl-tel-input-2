b72d1a6a717f1345143ba42d9381c9b69eecc364
Igor Petrovic <ipetrovic@vast.com>
2019-09-16 23:16:07 +0200

    init commit - without any custom stuff

---
+++ config/jest/cssTransform.js
@@ -0,0 +1,16 @@
+// This is a custom Jest transformer turning style imports into empty objects.
+// http://facebook.github.io/jest/docs/tutorial-webpack.html
+
+module.exports = {
+  process() {
+    return `
+      const idObj = require('identity-obj-proxy');
+      module.exports = idObj;
+    `;
+  },
+  getCacheKey() {
+    // eslint-disable-line no-unused-vars
+    // The output is always the same.
+    return 'cssTransform';
+  },
+};
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,187 @@
+/* eslint-disable no-var, arrow-parens, prefer-template, comma-dangle, object-shorthand, global-require, func-names, no-else-return, vars-on-top */
+const webpack = require('webpack');
+const paths = require('./paths');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const safeParser = require('postcss-safe-parser');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const getClientEnvironment = require('./env');
+
+// Webpack uses `publicPath` to determine where the app is being served from.
+// In development, we always serve from the root. This makes config easier.
+const publicPath = '';
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
+const publicUrl = '';
+// Get environment variables to inject into our app.
+const env = getClientEnvironment(publicUrl);
+
+// Assert this just to be safe.
+// Development builds of React are slow and not intended for production.
+if (env['process.env'].NODE_ENV !== '"production"') {
+  throw new Error('Production builds must have NODE_ENV=production.');
+}
+
+module.exports = {
+  mode: 'production',
+  devtool: false,
+  entry: {
+    main: './src/components/IntlTelInput.js',
+  },
+  output: {
+    path: paths.appDist,
+    pathinfo: true,
+    filename: '[name].js',
+    chunkFilename: '[name].bundle.js',
+    publicPath: publicPath,
+    library: 'IntlTelInput',
+    libraryTarget: 'umd',
+    globalObject: env.getGlobalObject(),
+  },
+
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+    'prop-types': {
+      root: 'PropTypes',
+      commonjs2: 'prop-types',
+      commonjs: 'prop-types',
+      amd: 'prop-types',
+    },
+    'libphonenumber-js-utils': {
+      root: 'intlTelInputUtils',
+      commonjs2: 'libphonenumber-js-utils',
+      commonjs: 'libphonenumber-js-utils',
+      amd: 'libphonenumber-js-utils',
+    },
+  },
+
+  resolve: {
+    modules: ['src', 'node_modules', ...paths.nodePaths],
+    alias: {
+      'react-intl-tel-input': './components/IntlTelInput.js',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: paths.appSrc,
+      },
+      {
+        exclude: [
+          /\.html$/,
+          /\.(js|jsx)$/,
+          /\.css$/,
+          /\.scss$/,
+          /\.json$/,
+          /\.png$/,
+          /\.svg$/,
+        ],
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'media/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: paths.appSrc,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader?outputStyle=expanded',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              pngquant: {
+                quality: '30-40',
+                speed: 1,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: true,
+          ecma: 6,
+          mangle: true,
+          output: {
+            comments: false,
+            beautify: false,
+          },
+        },
+        sourceMap: false,
+      }),
+      new OptimizeCssAssetsPlugin({
+        cssProcessorOptions: {
+          parser: safeParser,
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      }),
+    ],
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.DefinePlugin(env),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'report.html',
+      defaultSizes: 'parsed',
+      openAnalyzer: true,
+      generateStatsFile: false,
+      logLevel: 'info',
+    }),
+  ],
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+};
